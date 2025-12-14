33490bad588185c6392453eca0383c020e27e7e6
vishal7651 <vishalkshirsagar992@gmail.com>
2025-12-15 00:55:22 +0530

    Packages in Java

---
+++ package1/Package1Example.java
@@ -0,0 +1,14 @@
+package package1;
+public class Package1Example {
+    private int x;
+    private int y;
+
+    public void setValues(int p, int q){
+        x=p;
+        y=q;
+    }
+    public void display(){
+        System.out.println("\nThe Value of X is : "+x);
+        System.out.println("\nThe Value of X is : "+y);
+    }
+}
