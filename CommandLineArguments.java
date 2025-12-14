837916330feb4715e8b81757760c8abefde78203
vishal7651 <vishalkshirsagar992@gmail.com>
2025-12-15 00:07:18 +0530

    CommandLineArguments

---
+++ CommandLineArguments.java
@@ -0,0 +1,19 @@
+public class CommandLineArguments {
+    public static void main(String[] args) {
+        System.out.println("\nFirst Argument: "+args[0]);
+        System.out.println("\nSecond Argument: "+args[1]);
+        System.out.println("\nThird Argument: "+args[2]);
+        System.out.println("\nFourth Argument: "+args[3]);
+        System.out.println("\nFifth Argument: "+args[4]);
+
+        //For pronting all Arguments
+
+        int i, a=0;
+        for(i=0;i<args.length;i++){
+            System.out.println(args[i]);
+            a=a+Integer.parseInt(args[i]);
+        }
+                    System.out.println("value of a: "+a);
+
+    }
+}
--- WrapperClass.java
+++ WrapperClass.java
@@ -1,11 +1,15 @@
 public class WrapperClass {
     public static void main(String[] args) {
-        int x= Integer.parseInt("123");   
+        int x= Integer.parseInt("123");  
+        System.out.println(x);
+        
         Integer x1=Integer.valueOf("1001101",2);
+        System.out.println(x1);
         int y = x1.intValue();
 
-        System.out.println(x);
         System.out.println(y);
+
+        
 
     }
 }
