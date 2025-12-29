211ab23fd419cb1f9febcfe4d8ff1ba57d773dd0
vishal7651 <vishalkshirsagar992@gmail.com>
2025-12-29 13:01:39 +0530

    Formating

--- MultipleInheritanceUsingInterface.java
+++ MultipleInheritanceUsingInterface.java
@@ -1,33 +1,31 @@
-interface A{
-    default void show(){
-System.out.println("Show Method Call which present in A ");
-
+interface A {
+    default void show() {
+        System.out.println("Show Method Call which present in A ");
     };
 
 }
 
-interface B{
-default void show(){
-System.out.println("Show Method Call which present in B ");
+interface B {
+    default void show() {
+        System.out.println("Show Method Call which present in B ");
 
-};
+    };
 }
 
-class C implements A,B{
+class C implements A, B {
     @Override
-    public void show(){
-        A.super.show();     //  It choose A 
+    public void show() {
+        A.super.show(); // It choose A
 
-        B.super.show();     // It choose B
+        B.super.show(); // It choose B
     }
-    
+
 }
 
 public class MultipleInheritanceUsingInterface {
     public static void main(String[] args) {
-        
-    
-    C c = new C();
- c.show();
+
+        C c = new C();
+        c.show();
     }
 }
