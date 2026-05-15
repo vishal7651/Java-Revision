cbcda9059f81e5329696f97cd10788912c2495f7
vishal7651 <vishalkshirsagar992@gmail.com>
2026-05-15 09:46:44 +0530

    Exception Handling  try - Catch

--- ExceptionTryCatch.java
+++ ExceptionTryCatch.java
@@ -1,16 +1,32 @@
 public class ExceptionTryCatch {
     public static void main(String[] args) {
-        
-  
-    try{
-        int a = 10/0;
-        String b = null;
-        System.out.println(b.length());
-        // System.out.println(c);
-    }catch(ArithmeticException e1){
-        System.out.println(e1);
-    }catch(NullPointerException e2){
-        System.out.println(e2);
+
+        try {
+            int a = 10 / 0;
+
+            // System.out.println(c);
+        } catch (ArithmeticException e1) {
+            System.out.println(e1);
+        }
+        try {
+            String b = null;
+            System.out.println(b.length());
+        } catch (NullPointerException e2) {
+            System.out.println(e2);
+        }
+        try {
+
+            int[] arr = new int[2];
+
+            arr[5] = 10;
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Arithmetic");
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+            System.out.println("Array Error");
+        }
     }
-      }
 }
